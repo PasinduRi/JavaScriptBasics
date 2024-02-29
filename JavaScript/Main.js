d12deb23d53981278a47ee62e6d5232821b1abd5
PasinduRi <161703170+PasinduRi@users.noreply.github.com>
2024-02-29 15:59:46 +0530

    Add files via upload

---
+++ JavaScript/Main.js
@@ -0,0 +1,40 @@
+// scope of variables
+
+{
+    var x=5;
+    let y=6;
+    const z=7;
+}
+
+alert(x)
+alert(y)
+alert(z)
+
+//only x is  accessible outside the block, because it's  declared as var
+
+
+// Only Var can Redeclare
+
+var a=5;
+var a=7;
+
+
+alert(a)
+
+
+// Var is only Hositing
+
+carName = "Volvo";
+var carName;
+
+// const only the type that cannot reassign
+
+const PI = 3.14159265359;// const must be assigned when it declared.
+
+// const use when it declare arrays
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+
+
+
